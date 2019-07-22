57ca90acee6118646f401f13559d50ef829f6b03
edwardedd <eddmos86@gmail.com>
2019-07-22 12:30:12 +0300

    add javascript

--- weather.js
+++ weather.js
@@ -1,21 +1,40 @@
-window.addEventListener('load', ()=> {
-  let long;
-  let lat;
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position =>{
+const city = document.getElementById('name_city').value;
+const API ="https://api.openweathermap.org/data/2.5/weather?q=";
+
+// const API = 'https://jsonplaceholder.typicode.com/';
+
+
+
+
+
+const weatherBallon = async () => {
+  // const users = await getUsers();
+  const city = document.getElementById('name_city').value;
+  console.log(city)
+
+  // const getUsers = () => {
+    return fetch(API+city+"&units=metric&appid=952c0f2355e0268ba4082ae2cfcf5d64").then(res => {
+      return res.json();
+      console.log(res)
+    }).then(data => {
+      const temp = document.querySelector(".temp_degree");
+      temp.innerHTML = `
+        <h4>${data.main.temp}</h4>
+      `;
+      const icon = document.getElementById("wicon");
       
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      console.log(long)
-      console.log(lat)
-
-      const api = `https://api.darksky.net/forecast/f8f82802a688f76ce3ae8ea995de55cc/${lat},${long}`
+      icon.src = "http://openweathermap.org/img/w/"+ data.weather[0].icon+".png"
+      console.log(`${data.weather[0].icon} `)
+      // icon.src = `http://openweathermap.org/img/w/10d.png`
+      ;
+    }).catch(err => {
+      console.log('error')
     })
-  }else{
-    h1.textContent="not working"
-  }
+  // };
+ 
+}
 
-  fetch(api)
-      .then(data)
-})
+// weatherBallon()
+
+
