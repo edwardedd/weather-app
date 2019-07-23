c57994c63cf89f4c065ecdf9600b8eb4a0e9a1d4
edwardedd <eddmos86@gmail.com>
2019-07-23 16:11:22 +0300

    add css

--- weather.js
+++ weather.js
@@ -2,39 +2,60 @@
 const city = document.getElementById('name_city').value;
 const API ="https://api.openweathermap.org/data/2.5/weather?q=";
 
-// const API = 'https://jsonplaceholder.typicode.com/';
 
 
 
 
 
-const weatherBallon = async () => {
-  // const users = await getUsers();
-  const city = document.getElementById('name_city').value;
-  console.log(city)
 
-  // const getUsers = () => {
+const weatherBallon = async () => {  
+  const city = document.getElementById('name_city').value;  
     return fetch(API+city+"&units=metric&appid=952c0f2355e0268ba4082ae2cfcf5d64").then(res => {
       return res.json();
       console.log(res)
     }).then(data => {
       const temp = document.querySelector(".temp_degree");
       temp.innerHTML = `
-        <h4>${data.main.temp}</h4>
+        <h4>Temperature:${data.main.temp} C</h4>
+        
       `;
-      const icon = document.getElementById("wicon");
-      
+      const wind = document.querySelector(".wind");
+      wind.innerHTML = `
+        <h4>Wind speed:${data.wind.speed} m/s</h4>
+      `;
+      const icon = document.getElementById("wicon");      
       icon.src = "http://openweathermap.org/img/w/"+ data.weather[0].icon+".png"
-      console.log(`${data.weather[0].icon} `)
-      // icon.src = `http://openweathermap.org/img/w/10d.png`
-      ;
-    }).catch(err => {
+      console.log(`${data.weather[0].icon} `);    
+      }).catch(err => {
       console.log('error')
-    })
-  // };
- 
+       const error = document.getElementById("result");
+       error.innerHTML = `
+        <h4>incorect name of the city</h4>
+      `;
+      });
 }
 
-// weatherBallon()
+$.getJSON('data.json', function(data){
+  console.log(data.id)
+})
+
+// $(document).ready(function(){
+//   $('#name_city').keyup(function(){
+//     $('result').html('');
+//     var searchFiled = $('#name_city').val();
+//     var expression = new RegExp(searchFiled, "i");
+//     $.getJSON('data.json', function(data){
+//       $.each(data, function(key,value){
+//         if(value.name.search(expression) != -1 )
+//         {
+//           $("result").append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.location+'</span></li>')
+//         }
+//       })
+//     })
+//   })
+// })
+
+
+
 
 
